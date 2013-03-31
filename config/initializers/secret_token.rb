fbb36eb1bdcceb648ed3299e6c7ce4cc408df5ae
Richard <richard@mozor.net>
2013-03-31 13:22:53 -0500

    Initial scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hlwkl::Application.config.secret_token = 'c1151ed472946d1402e93107a2d289a1d611cd6acbe915f9d3a9d770830817d0054642575a44988efd30ffd021881e84b962b39933e9af02e694194da1b7b828'
